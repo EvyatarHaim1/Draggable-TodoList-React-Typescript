8211c13fcf255bdd7c0ea248b2071ca678867df3
Evyatar Haim <evyataros1@gmail.com>
2022-03-13 09:26:04 +0200

    adding snapshot & draggable styles

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">Evyatar Haim - Daily Todo</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
